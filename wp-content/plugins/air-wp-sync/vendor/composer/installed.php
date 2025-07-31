cd80db3f4007a27c953a169836038e4fcbe1e55b
Hamo <Hamo.Miracle@gmail.com>
2025-07-31 13:08:26 +0900

    second

---
+++ wp-content/plugins/air-wp-sync/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '17f36034a15b67813e206a161ecbf2666b6b6410',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '17f36034a15b67813e206a161ecbf2666b6b6410',
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.5.4',
+            'version' => '3.5.4.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'reference' => '9533e71b0eba4a519721dde84a34dfb161f11eb8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
